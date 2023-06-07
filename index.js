001f2c550a40a009d2290468192c68c3ff79cc92
muhiminulalvi <eee1916039brur@gmail.com>
2023-06-08 01:11:18 +0600

    cached node_modules

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
-
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 /*
 ===================================================
@@ -17,6 +18,58 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
+
+/*
+===================================================
+            MongoDB
+===================================================
+*/
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kkbgyge.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const instructorCollection = client.db("photoSafari").collection("instructors");
+    const classCollection = client.db("photoSafari").collection("classes");
+
+    // instructors data
+    app.get('/instructors', async(req,res)=>{
+        const result = await instructorCollection.find().toArray()
+        res.send(result)
+    })
+
+    // classes data
+    app.get('/classes', async(req,res)=>{
+        const result = await classCollection.find().toArray()
+        res.send(result)
+    })
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res)=>{
     res.send('The server is running')
